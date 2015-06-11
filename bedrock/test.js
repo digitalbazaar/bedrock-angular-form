389e38cc153c1d9f7aa1d32e38107dfabfb5b057
Dave Longley <dlongley@digitalbazaar.com>
2015-06-10 22:22:08 -0400

    Add manifests and test file for running unit tests.

---
+++ bedrock/test.js
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2015 Digital Bazaar, Inc. All rights reserved.
+ */
+var bedrock = require('bedrock');
+var fs = require('fs');
+var path = require('path');
+
+require('bedrock-express');
+require('bedrock-protractor');
+require('bedrock-views');
+
+var config = bedrock.config;
+var dir = path.join(__dirname, '..');
+config.requirejs.bower.packages.push({
+  path: dir,
+  manifest: JSON.parse(fs.readFileSync(
+    path.join(dir, 'bower.json'), {encoding: 'utf8'}))
+});
+
+bedrock.start();
