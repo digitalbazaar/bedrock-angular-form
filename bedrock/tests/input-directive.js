f364a5e691b24211f507ce1f81bbe69ee8b1e062
Dave Longley <dlongley@digitalbazaar.com>
2015-06-12 00:56:38 -0400

    Use bedrock-protractor `testInBrowser` feature.
    - Refactor unit tests to run in mocha in the browser.

--- bedrock/tests/input-directive.js
+++ bedrock/tests/input-directive.js
@@ -1,76 +1,44 @@
 var bedrock = GLOBAL.bedrock;
 
-describe('br-input', function() {
+bedrock.testInBrowser('br-input', function($injector) {
+
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('unit tests', function() {
+  var $compile = $injector.get('$compile');
+  var $rootScope = $injector.get('$rootScope');
+  var $scope, element;
+
   beforeEach(function() {
-    bedrock.waitForAngular();
+    $scope = $rootScope.$new();
+    element = $compile('<div></div>')($scope);
+  });
+
+  afterEach(function() {
+    element.remove();
   });
 
   it('should transclude help text in legacy mode', function() {
-    expect(bedrock.run(function($injector) {
-      // base setup
-      var result;
-      var $compile = $injector.get('$compile');
-      var $rootScope = $injector.get('$rootScope');
-      var $scope = $rootScope.$new();
-      var element = $compile('<div></div>')($scope);
-
-      // test setup
-      element.append($compile('<br-input>help</br-input>')($scope));
-      $scope.$apply();
-
-      // expected result
-      result = element.find('.help-block').text().trim();
-
-      // clean up
-      element.remove();
-      return result;
-    })).to.eventually.equal('help');
+    element.append($compile('<br-input>help</br-input>')($scope));
+    $scope.$apply();
+    expect(element.find('.help-block').text().trim()).to.equal('help');
   });
 
   it('should transclude help text', function() {
-    expect(bedrock.run(function($injector) {
-      // base setup
-      var result;
-      var $compile = $injector.get('$compile');
-      var $rootScope = $injector.get('$rootScope');
-      var $scope = $rootScope.$new();
-      var element = $compile('<div></div>')($scope);
-
-      // test setup
-      element.append($compile(
-        '<br-input><div name="br-input-help">help</div></br-input>')($scope));
-      $scope.$apply();
-
-      // expected result
-      result = element.find('.help-block').text().trim();
-
-      // clean up
-      element.remove();
-      return result;
-    })).to.eventually.equal('help');
+    element.append($compile(
+      '<br-input><div name="br-input-help">help</div></br-input>')($scope));
+    $scope.$apply();
+    expect(element.find('.help-block').text().trim()).to.equal('help');
   });
 
   it('should transclude help text and ignore non-help content', function() {
-    expect(bedrock.run(function($injector) {
-      // base setup
-      var result;
-      var $compile = $injector.get('$compile');
-      var $rootScope = $injector.get('$rootScope');
-      var $scope = $rootScope.$new();
-      var element = $compile('<div></div>')($scope);
-
-      // test setup
-      element.append($compile(
-        '<br-input><div name="br-input-help">help</div>ignore</br-input>')(
-          $scope));
-      $scope.$apply();
-
-      // expected result
-      result = element.find('.help-block').text().trim();
-
-      // clean up
-      element.remove();
-      return result;
-    })).to.eventually.equal('help');
+    element.append($compile(
+      '<br-input><div name="br-input-help">help</div>ignore</br-input>')(
+        $scope));
+    $scope.$apply();
+    expect(element.find('.help-block').text().trim()).to.equal('help');
   });
-});
+}); // end unit tests
+
+}); // end client mocha
