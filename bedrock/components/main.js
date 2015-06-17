791fcee208a7acb638a2a21a92d3d0b74ae276e3
Dave Longley <dlongley@digitalbazaar.com>
2015-06-16 21:23:15 -0400

    Add test bower package.

---
+++ bedrock/components/main.js
@@ -0,0 +1,9 @@
+define(['angular'], function(angular) {
+
+'use strict';
+
+var module = angular.module('bedrock-angular-form-test', []);
+
+return module.name;
+
+});
--- bedrock/test.js
+++ bedrock/test.js
@@ -11,6 +11,8 @@ require('bedrock-views');
 
 var config = bedrock.config;
 var dir = path.join(__dirname, '..');
+
+// add bedrock-angular-form bower package
 config.requirejs.bower.packages.push({
   path: dir,
   manifest: JSON.parse(fs.readFileSync(
@@ -18,5 +20,18 @@ config.requirejs.bower.packages.push({
 });
 
 config.views.paths.push(path.join(__dirname, 'views'));
+
+// add test bower package
+config.requirejs.bower.packages.push({
+  path: path.join(dir, 'components'),
+  manifest: {
+    name: 'bedrock-angular-form-test-components',
+    moduleType: 'amd',
+    main: './main.js',
+    dependencies: {
+      angular: '~1.3.0'
+    }
+  }
+});
 
 bedrock.start();
