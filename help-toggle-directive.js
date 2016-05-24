978404d933c72d91e47c186cfe3a750bb0017ced
Dave Longley <dlongley@digitalbazaar.com>
2016-05-24 12:21:15 -0400

    Add deprecation notice.

--- help-toggle-directive.js
+++ help-toggle-directive.js
@@ -1,6 +1,8 @@
 /*!
  * Help Toggle directive.
  *
+ * FIXME: DEPRECATED - remove in major version 3
+ *
  * Copyright (c) 2012-2014 Digital Bazaar, Inc. All rights reserved.
  *
  * @author Dave Longley
